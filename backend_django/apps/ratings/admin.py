fcfe30449fac473f9351199b4565b28f56c9bd1c
sammy ocharo <obanyisammy@gmail.com>
2022-03-08 13:24:56 +0300

    added rating model

--- backend_django/apps/ratings/admin.py
+++ backend_django/apps/ratings/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from.models import Rating
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ["rater", "agent", "rating"]
+
+admin.site.register(Rating, RatingAdmin)
+
